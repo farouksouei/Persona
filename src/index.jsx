73f1a5b5f99c21bcb4c3126c70007938416bcd1b
Farouk Souei <farouksouei@gmail.com>
2023-08-24 21:00:16 +0100

    2nd screen

--- src/index.jsx
+++ src/index.jsx
@@ -5,9 +5,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/index.css";
 import "./styles/tailwind.css";
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <DevSupport ComponentPreviews={ComponentPreviews}
+                    useInitialHook={useInitial}
+        >
+            <App/>
+        </DevSupport>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
